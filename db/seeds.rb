fca7ca658e126df61bb49da9411de1c00705f3e8
Diana Rybikov <diana.ryb214@gmail.com>
2021-11-04 17:11:32 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Pirates.destroy_all
+
+Pirates.create(
+    name: 'Jack Sparrow',
+    age:"36",
+    ship: "Black Pearl"
+)
+Pirates.create(
+    name: 'Black Beard',
+    age:"39",
+    ship: "Queen Anne's Revenge"
+)
+Pirates.create(
+    name: 'Captain Hook',
+    age:"40",
+    ship: "Jolly Roger"
+)
